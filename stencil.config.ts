acfd54b5a982ff855d50d2c6eb31a813a0eb4f6a
bitflower <matthias.max@bitflower.net>
2019-09-04 09:38:39 +0200

    showcase error

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,13 @@ import { Config } from '@stencil/core';
 
 // https://stenciljs.com/docs/config
 
+const copiedFiles: any[] = [
+  {
+    src: '../node_modules/@stencil/core/dist/cli', // TOTALLY RANDOM SOURCE JUST TO SHOWCASE ANY COPY ACTIVITY INTO "assets"
+    dest: 'assets'
+  }
+];
+
 export const config: Config = {
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
@@ -10,7 +17,8 @@ export const config: Config = {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/'
+      baseUrl: 'https://myapp.local/',
+      copy: copiedFiles
     }
   ]
 };
